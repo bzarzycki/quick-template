8eea4d96f6eb3357d2a2c9c272b9cf5e46edaf43
bzarzycki <blazej.zarzycki@gmail.com>
2017-06-17 12:54:47 +0200

    new functions

--- quick-template.js
+++ quick-template.js
@@ -1,18 +1,59 @@
+/**
+ * @author Blazej Zarzycki
+ */
 function QuickTemplate() {
 
-    var template = "";
+    var placeholder = {
+        left: '[[',
+        right: ']]'
+    }
+    
+    var attributePrefix = 'data-tmpl-';
 
-    this.setTemplate = function (tmpl) {
-        template = tmpl;
+    this.setPlaceholderSymbol = function (left, right) {
+        placeholder.left = open;
+        placeholder.right = close;
     }
 
-    this.getView = function (model) {
+    this.setAttributePrefix = function (prefix) {
+        attributePrefix = prefix;
+    }
+
+    this.process = function (template, model) {
         var output = template;
         for (var key in model) {
-            output = output.replace('[[' + key + ']]', model[key]);
+            var str = placeholder.left + key + placeholder.right;
+            output = output.replace(str, model[key]);
         }
-        output = output.replace('data-tmpl-', '');
+        output = output.replace(attributePrefix, '');
         return output;
+    }
+
+    this.randomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    this.randomFloat = function (min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    this.randomByMask = function (mask) {
+        var output = '';
+        for (var i = 0; i < mask.length; i++) {
+            if (mask[i] === '_') {
+                output += this.randomInt(0, 9);
+            } else {
+                output += mask[i];
+            }
+        }
+        return output;
+    }
+
+    this.randomStr = function (buffer, size) {
+        var buffLength = buffer.length;
+        var first = this.randomInt(0, buffLength - size);
+        var last = first + size;
+        return buffer.substring(first, last);
     }
 
 }
